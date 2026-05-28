33cd5058bc8d7c01c20b8cc1ed0c4273c1ab534c
Aerousme21 <avalent211@gmail.com>
2026-05-29 05:32:23 +0700

    Add admin web panel served from Laravel public folder

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/{any?}', function () {
+    $path = public_path('admin/index.html');
+    if (!file_exists($path)) {
+        abort(404, 'Admin panel not built.');
+    }
+    return response()->file($path);
+})->where('any', '.*');
